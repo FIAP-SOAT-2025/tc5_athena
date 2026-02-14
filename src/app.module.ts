774fc5b4d0594cf5052c677f3148419156c3a6c1
Daniela-Rego <danielalimaqueiroz@gmail.com>
2026-02-14 17:08:05 -0300

    add Swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { VideoModule } from './video/video.module';
 import { UsersModule } from './users/users.model';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
 
 @Module({
-  imports: [VideoModule, UsersModule],
+  imports: [ ConfigModule.forRoot(),VideoModule, UsersModule],
   controllers: [],
   providers: [],
 })
