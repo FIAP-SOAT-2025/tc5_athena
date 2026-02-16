cbfd1aa745a12ea8c0314cd104102a1e9ba4f2f2
DianaBianca <dianabiancasr@gmail.com>
2026-02-16 13:22:27 -0300

    WIP

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { VideoModule } from './video/video.module';
 import { UsersModule } from './users/users.model';
 import { StorageModule } from './storage/storage.module';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
-  imports: [VideoModule, UsersModule, StorageModule],
+  imports: [
+    PrometheusModule.register(),
+    VideoModule,
+    UsersModule,
+    StorageModule
+  ],
   controllers: [],
   providers: [],
 })
