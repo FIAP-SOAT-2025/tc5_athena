ba93fb379b6fd30dd0fa40670c58ded512f582bd
DianaBianca <dianabiancasr@gmail.com>
2026-03-10 22:41:05 -0300

    ajustes var terraform

--- iac/terraform/vars.tf
+++ iac/terraform/vars.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "projectName" {
   description = "The name of the project"
-  default     = "tc5-g192-athena-v1"
+  default     = "tc5-g192-athena"
 }
 
 variable "grafana_admin_user" {
